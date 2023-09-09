9bcf7f600ff2c89029da8ce23d28e4b0e9b32ad1
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 20:46:55 +0000

    add file EX2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - print all alphabet in lowercase.
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
     char alp[26] = "abcdefghijklmnopqrstuvwxyz";
