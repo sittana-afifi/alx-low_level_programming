591356ea7029abfd32233a60c394642456cd4b81
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 22:11:00 +0000

    add file EX9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all digits.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{   char num[16] = "0123456789";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(num[i]);
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
