1c94e5a7ccddae1ebecd0abddc35a4c00b953bbe
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 21:49:16 +0000

    add file EX7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,7 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{   char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+{   
+    char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
