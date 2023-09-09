ba18f1d36c22fb0eabc9ccb49b5c0a020ad0f24b
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 11:55:58 +0000

    add file EX0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n, l;
