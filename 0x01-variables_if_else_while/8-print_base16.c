4cfc887f883db4495ea2a7c2a100ec2dd0748ad6
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 22:07:15 +0000

    add file EX8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all digits.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{   char num[16] = "0123456789101112131415";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(num[i]);
+	}
+	putchar('\n');
+	return (0);
+}
