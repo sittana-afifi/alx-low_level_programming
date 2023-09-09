4aef13308ac1429e3e187c87c470426bccc603ba
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 21:25:20 +0000

    add file EX6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{   char num[10] = "0123456789";
+{   
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(num[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
