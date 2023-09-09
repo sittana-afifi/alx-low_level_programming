cb026090b3332ef0dc897b42c934b1906ec6c6df
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 22:41:40 +0000

    add file EX9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,11 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-        if (i != 57)
-    {
-		    putchar(',');
-		    putchar(' ');
-    }
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
