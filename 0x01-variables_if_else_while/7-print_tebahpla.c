1b742360965e51b210e46aed516e699c7b1524e3
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-09 21:38:10 +0000

    Add file EX7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - print all alphbet in reverse.
  * Return: Always 0 (Success)
  */
-
 int main(void)
-{   
-    char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+{   char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 	for (i = 0; i < 26; i++)
 	{
