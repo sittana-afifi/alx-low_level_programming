46d19ddb45c2f3bc6c8a8453266cc42611a3e77c
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-12 20:17:32 +0000

    add file EX0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,9 +10,9 @@ int main(void)
 _putchar('p');
 _putchar('u');
 _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0); }
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0); }
