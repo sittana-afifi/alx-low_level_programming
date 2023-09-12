623133e858ebb5ecc3f206e280406ffde193d11a
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-12 20:25:23 +0000

    add file EX2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
-  * print_alphabet_x10 - Make alphabet x10 times
-  *
-  * Return: void
-  */
-void print_alphabet_x10(void)
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
 {
-	char c;
-	int i = 0;
+	int i;
 
-	while (i <= 9)
+	for (i = 97; i <= 122; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-
-		_putchar('\n');
-
-		i++;
+		_putchar(i);
 	}
+	_putchar('\n');
 }
