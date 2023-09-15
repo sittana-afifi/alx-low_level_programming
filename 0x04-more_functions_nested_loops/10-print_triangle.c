2982e2e9ae2aa323469d691a938ac7bc4eeddf97
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-15 19:00:38 +0000

    add file Ex

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle.
+ * @size: numbers of lines.
+ * Return: no return.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
