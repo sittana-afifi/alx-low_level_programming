9982150fa499a15d89910f9fb7522db2375ab598
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-15 19:31:54 +0000

    add file EX

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,6 +10,11 @@ void print_triangle(int n)
 	int i, j;
 
 	for (i = 0; i < n; i++)
+	{
+		for (j = 1; j < (n - i); j++)
+			_putchar(' ');
+		for (j--; j < n; j++)
+			_putchar(35);
 		if (i < (n - 1))
 			_putchar('\n');
 	}
