005e75004c8f38c75ac484e1749afbe3bc81a604
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-18 17:20:26 +0000

    add file EX6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string!
+ * @str: input string.
+ * Return: no return.
+ */
+void puts2(char *str)
+{
+	int c = 0;
+
+	while (c >= 0)
+	{
+		if (str[c] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (c % 2 == 0)
+			_putchar(str[c]);
+		c++;
+	}
+}
