ead965fdd79f2d866e9c118e1a950c93e280c4d3
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2023-09-25 23:53:52 +0000

    add fileEx

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _memcpy - copies memory area,
- * @dst: dstination memory area.
+ * @dest: destination memory area.
  * @src: source memory area.
  * @n: bytes filled.
- * Return: the pointer to dst.
+ * Return: the pointer to dest.
  */
-char *_memcpy(char *dst, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*(dst + i) =  *(src + i);
+		*(dest + i) =  *(src + i);
 
-	return (dst);
+	return (dest);
 }
